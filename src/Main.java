bb872642b31a318c396d4d6f98034b130c5aa913
sakthiveljoker <77488929+sakthiveljoker@users.noreply.github.com>
2021-08-18 11:25:43 +0530

    NumberToWords

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        NumberToWords.numberToWords(0);
+        System.out.println(NumberToWords.reverse(123));
+        System.out.println(NumberToWords.getDigitCount(123));
+    }
+}
